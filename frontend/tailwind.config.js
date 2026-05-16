ef7e1c0859df10eda7a7c70139754a888259a362
Aryan Sahu <aryansahu0004@gmail.com>
2026-05-16 10:44:53 +0530

    chore: scaffold frontend tooling

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#172033',
+        forge: '#315EFB',
+        mint: '#11A683',
+        amber: '#E39D26'
+      }
+    }
+  },
+  plugins: []
+};
